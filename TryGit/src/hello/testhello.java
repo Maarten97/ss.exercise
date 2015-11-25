cdd77b6468ae51ddd30262d27ebcbc4a4d430f61
Maarten97 <m.s.looijenga@student.utwente.nl>
2015-11-25 13:04:22 +0100

    D-3.8

--- TryGit/src/hello/testhello.java
+++ TryGit/src/hello/testhello.java
@@ -2,10 +2,7 @@ package hello;
 
 public class testhello {
 	// hallo //
-<<<<<<< master
+
 	// Dit is om Hello.java te editten//
 	//test
-=======
-	
->>>>>>> 1e8e3e0 Dit is om uit te testen hoe je een versie weer terugzet
 }
