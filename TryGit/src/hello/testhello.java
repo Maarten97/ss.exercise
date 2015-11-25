9ace1cfb6af541ee32c7a0d1ebdf3612f762db68
Maarten97 <m.s.looijenga@student.utwente.nl>
2015-11-25 13:00:38 +0100

    Opnieuw uploaden

--- TryGit/src/hello/testhello.java
+++ TryGit/src/hello/testhello.java
@@ -2,6 +2,10 @@ package hello;
 
 public class testhello {
 	// hallo //
+<<<<<<< master
 	// Dit is om Hello.java te editten//
 	//test
+=======
+	
+>>>>>>> 1e8e3e0 Dit is om uit te testen hoe je een versie weer terugzet
 }
--- TryGit/src/hello2/test2.java
+++
@@ -1,5 +0,0 @@
-package hello2;
-
-public class test2 {
-	// hello voor test 2 //
-}
