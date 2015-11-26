6ca2d6a34cefabad1a9190864fac0bc7de2f0ef9
gijs <thijsons@live.nl>
2015-11-26 15:47:07 +0100

    for life!

--- TryGit/src/hello/Hello.java
+++ TryGit/src/hello/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		//hello Maarten! -Gijs//
 		//Ich bin ein Berliner...   Berlinerbol!!!
+		//berlinerbollen for life!
 		
 	}
 	public Bear(int legs, boolean tail, String color, boolean awake) {
